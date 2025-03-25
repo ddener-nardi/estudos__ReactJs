5a829ca59d9fe9039901cbabef43c3466e450fc9
Dener Nardi <monarchianardi@gmail.com>
2025-03-25 20:07:01 -0300

    feat: Iniciando estudos de ReactJs;

---
+++ fundamentos/src/App.jsx
@@ -0,0 +1,14 @@
+import FirstComponent from './components/FirstComponent'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Fundamentos ReactJs</h1>
+      <FirstComponent></FirstComponent>
+    </div>
+  )
+}
+
+export default App
---
+++ fundamentos/src/components/FirstComponent.jsx
@@ -0,0 +1,14 @@
+// arquivos de estilo do componente
+
+
+const FirstComponent = () => {
+    //essa função funciona...
+    return (
+        <div>
+            {/* Algum comentário */}
+            <h1>Meu Primeiro Componente!</h1>
+        </div>
+    );
+}
+
+export default FirstComponent;
